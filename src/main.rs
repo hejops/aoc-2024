749d5f6aee05bbf7c23c810ce94ed6a0010161c7
Joseph <hejops1@gmail.com>
2024-12-02 19:58:14 +0100

    add boilerplate for remaining days

--- src/main.rs
+++ src/main.rs
@@ -100,3 +100,118 @@ fn day2() {
         .count();
     println!("{:#?}", safe);
 }
+
+fn day3() {
+    //{{{
+    let contents = fs::read_to_string("day-3-input.txt").unwrap();
+} //}}}
+
+fn day4() {
+    //{{{
+    let contents = fs::read_to_string("day-4-input.txt").unwrap();
+} //}}}
+
+fn day5() {
+    //{{{
+    let contents = fs::read_to_string("day-5-input.txt").unwrap();
+} //}}}
+
+fn day6() {
+    //{{{
+    let contents = fs::read_to_string("day-6-input.txt").unwrap();
+} //}}}
+
+fn day7() {
+    //{{{
+    let contents = fs::read_to_string("day-7-input.txt").unwrap();
+} //}}}
+
+fn day8() {
+    //{{{
+    let contents = fs::read_to_string("day-8-input.txt").unwrap();
+} //}}}
+
+fn day9() {
+    //{{{
+    let contents = fs::read_to_string("day-9-input.txt").unwrap();
+} //}}}
+
+fn day10() {
+    //{{{
+    let contents = fs::read_to_string("day-10-input.txt").unwrap();
+} //}}}
+
+fn day11() {
+    //{{{
+    let contents = fs::read_to_string("day-11-input.txt").unwrap();
+} //}}}
+
+fn day12() {
+    //{{{
+    let contents = fs::read_to_string("day-12-input.txt").unwrap();
+} //}}}
+
+fn day13() {
+    //{{{
+    let contents = fs::read_to_string("day-13-input.txt").unwrap();
+} //}}}
+
+fn day14() {
+    //{{{
+    let contents = fs::read_to_string("day-14-input.txt").unwrap();
+} //}}}
+
+fn day15() {
+    //{{{
+    let contents = fs::read_to_string("day-15-input.txt").unwrap();
+} //}}}
+
+fn day16() {
+    //{{{
+    let contents = fs::read_to_string("day-16-input.txt").unwrap();
+} //}}}
+
+fn day17() {
+    //{{{
+    let contents = fs::read_to_string("day-17-input.txt").unwrap();
+} //}}}
+
+fn day18() {
+    //{{{
+    let contents = fs::read_to_string("day-18-input.txt").unwrap();
+} //}}}
+
+fn day19() {
+    //{{{
+    let contents = fs::read_to_string("day-19-input.txt").unwrap();
+} //}}}
+
+fn day20() {
+    //{{{
+    let contents = fs::read_to_string("day-20-input.txt").unwrap();
+} //}}}
+
+fn day21() {
+    //{{{
+    let contents = fs::read_to_string("day-21-input.txt").unwrap();
+} //}}}
+
+fn day22() {
+    //{{{
+    let contents = fs::read_to_string("day-22-input.txt").unwrap();
+} //}}}
+
+fn day23() {
+    //{{{
+    let contents = fs::read_to_string("day-23-input.txt").unwrap();
+} //}}}
+
+fn day24() {
+    //{{{
+    let contents = fs::read_to_string("day-24-input.txt").unwrap();
+} //}}}
+
+fn day25() {
+    //{{{
+    let contents = fs::read_to_string("day-25-input.txt").unwrap();
+} //}}}
