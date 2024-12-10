a83815274059d08d006fa2305141a32bc1fb1f73
Joseph <hejops1@gmail.com>
2024-12-10 19:24:16 +0100

    tidy

--- src/day10.rs
+++ src/day10.rs
@@ -33,20 +33,14 @@ pub fn main() {
 
     let get_neighbours = |pos: usize, value: u8| -> Option<HashSet<usize>> {
         let up = pos.checked_sub(cols);
-        let left = (pos % cols).gt(&0).then(|| pos.checked_sub(1).unwrap());
-        let right = {
-            if (pos % cols) < cols - 1 && pos + 1 < flat_len {
-                Some(pos + 1)
-            } else {
-                None
-            }
-        };
+        let left = (pos % cols).gt(&0).then_some(pos.checked_sub(1).unwrap());
+        let right = ((pos % cols) < cols - 1 && pos + 1 < flat_len).then_some(pos + 1);
         let down = (pos + cols).lt(&flat_len).then_some(pos + cols);
 
         let neighbours = [up, left, right, down]
             .into_iter()
             .flatten()
-            .filter(|dir| flat_grid[*dir] == value)
+            .filter(|neighbour| flat_grid[*neighbour] == value)
             .collect::<HashSet<usize>>();
 
         (!neighbours.is_empty()).then_some(neighbours)
@@ -59,14 +53,16 @@ pub fn main() {
         let start = pos;
         let mut positions = HashSet::from_iter(vec![pos]);
         let mut next_value = 1;
+
         while !positions.is_empty() && next_value <= 9 {
             positions = positions
                 .iter()
                 .filter_map(|pos| get_neighbours(*pos, next_value))
                 .flatten()
                 .collect::<HashSet<_>>();
-            next_value += 1;
+
             endpoints.insert(start, positions.clone());
+            next_value += 1;
         }
     }
 
@@ -74,23 +70,21 @@ pub fn main() {
     println!("{:?}", score);
 
     // part 2
-    let mut sum = 0;
-
-    for pos in zeros {
-        let route = vec![pos];
+    let sum = zeros.iter().fold(0, |sum, pos| {
+        let route = vec![*pos];
         let mut routes: Vec<Vec<usize>> = vec![route];
         let mut next_value = 1;
 
         while next_value <= 9 {
             let mut extended_routes: Vec<Vec<usize>> = vec![];
 
-            for route in &routes {
+            for route in &mut routes {
                 if let Some(neighbours) = get_neighbours(*route.last().unwrap(), next_value) {
-                    for n in neighbours {
-                        let mut extended_route: Vec<usize> = route.clone();
-                        extended_route.push(n);
-                        extended_routes.push(extended_route);
-                    }
+                    extended_routes = neighbours.iter().fold(extended_routes, |mut routes, n| {
+                        route.push(*n);
+                        routes.push(route.to_vec());
+                        routes
+                    });
                 };
             }
 
@@ -98,7 +92,8 @@ pub fn main() {
             next_value += 1;
         }
 
-        sum += routes.len();
-    }
+        sum + routes.len()
+    });
+
     println!("{:?}", sum);
 }
