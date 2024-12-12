04305d2d22060abd8dded00a0b7aecb446c48aa1
Joseph <hejops1@gmail.com>
2024-12-12 22:17:26 +0100

    day 12 part 1

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,81 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::ops::Sub;
+use std::usize;
+
 pub fn main() {
-    let contents = include_str!("../input/day-12-input.txt");
+    let contents = "RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE";
+
+    let contents = "AAAA
+BBCD
+BBCC
+EEEC";
+
+    // let contents = include_str!("../input/day-12-input.txt");
+
+    let rows = contents.lines().count();
+    let cols = contents.lines().next().unwrap().len();
+    // println!("{:?} {}", rows, cols); // 140*140
+
+    let flat_grid = contents.replace('\n', "").chars().collect::<Vec<char>>();
+
+    let get_neighbours = |pos: usize, c: &char| -> HashSet<usize> {
+        let up = pos.checked_sub(cols);
+        let left = ((pos % cols) > 0).then(|| pos - 1);
+        let right = ((pos % cols) + 1 < cols).then(|| pos + 1);
+        let down = ((pos / rows) + 1 < rows).then(|| pos + rows);
+
+        [up, left, right, down]
+            .into_iter()
+            .flatten()
+            .filter(|neighbour| flat_grid[*neighbour] == *c)
+            .collect::<HashSet<usize>>()
+    };
+
+    // part 1
+    // flood fill
+    let mut seen: HashSet<usize> = HashSet::new();
+    // { pos: {perimeter, perimeter, ...}, pos: ... }
+    let mut regions: HashMap<usize, Vec<usize>> = HashMap::new();
+
+    for (pos, c) in flat_grid.iter().enumerate() {
+        if seen.contains(&pos) {
+            continue;
+        }
+
+        let mut neighbours = get_neighbours(pos, c);
+        seen.insert(pos);
+        regions.insert(pos, vec![4.sub(neighbours.len())]);
+
+        while !neighbours.is_empty() {
+            let mut all_neighbours = HashSet::new();
+
+            for n in neighbours {
+                seen.insert(n);
+                let new = get_neighbours(n, c);
+                regions.entry(pos).and_modify(|v| v.push(4.sub(new.len())));
+                all_neighbours.extend(new.clone());
+            }
+
+            neighbours = all_neighbours
+                .into_iter()
+                .filter(|n| !seen.contains(n))
+                .collect();
+        }
+    }
+
+    let score = regions
+        .values()
+        .map(|r| r.len() * r.iter().sum::<usize>())
+        .sum::<usize>();
+    println!("{:?}", score);
 }
